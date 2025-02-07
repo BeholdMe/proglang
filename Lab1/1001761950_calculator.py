42895437f64ff530760e2fbf64c423b99b716996
Balaputradewa Ratuwina <be.ratuwina@gmail.com>
2025-02-06 18:29:35 -0600

    continued on python code

--- Lab1/1001761950_calculator.py
+++ Lab1/1001761950_calculator.py
@@ -1,3 +1,24 @@
+# create the functions to add two numbers and return sum
+def add(num1, num2):
+    return num1 + num2
+
+# create the functions to subtract number 2 from number 1 and return difference
+def subtract(num1, num2):
+    return num1 - num2
+
+# create the functions to multiply two numbers and return sum
+def multiply(num1, num2):
+    return num1 * num2
+
+# create the functions to divide number 1 by number 2 and return result
+def divide(num1, num2):
+    #make sure y is not 0 b/c that will cause an error
+    if num2 == 0.0:
+        return "You can't divide by 0 yet!"
+    else:
+        return num1 / num2
+
+
 def calculator():
     #create the number variables and operators to
     #make sure the computer know it is the right daata type (float and String)
@@ -5,6 +26,22 @@ def calculator():
     num2 = 0.0
     operator = ""
     answer = 0.0
+    inStr = ""
 
-    # ask the user for inputs (num1, num2)
-    
+    # keep asking user until either user quits or 
+    while True:
+        # ask the user for inputs (num1, num2)
+        inStr = input("Enter expression (or Q to quit): ")
+        if inStr == 'q' or inStr == 'Q':
+            break
+
+
+        try:
+            inStrAsSeparate = inStr.split(" ") # split by spaces
+        except IndexError:
+            print("Something went wrong with your expression")
+            continue
+
+        # Check that first and last elements in inStrAsSeparate are numbers (specifically floats)
+        # Check that the second element is one of +, -, *, /
+        # 
